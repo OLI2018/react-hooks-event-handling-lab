42f1f76f1ce101fd5723b4768824fcfc1e176eb9
Oleg <roxvemm2014@gmail.com>
2021-07-03 13:30:06 -0700

    commit tree 205cf66314ba8c82d813c8c84aa2c40a69ac3bac

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,24 @@
-// Code Keypad Component Here
+  
+import React from 'react'
+
+function Keypad() {
+
+  function handleEvent() {
+    console.log("Entering password...")
+  }
+
+  return (
+    <input type="password" onChange={handleEvent} />
+  )
+}
+
+export default Keypad
+
+
+
+
+
+
+
+
+
